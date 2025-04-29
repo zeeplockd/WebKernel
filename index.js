56f4cfbf07dd4f9f144cfb255194745621ca74f7
callum <master.callum.burrows@gmail.com>
2025-04-29 13:01:17 +0100

    made seperate functions for formatted and non formatted text

--- index.js
+++ index.js
@@ -19,28 +19,23 @@ const colorMap = {
 };
 
 class WebKernel {
-    static stringOut(stringToPut) {
+    static formattedStringOut(stringToPut) {
         var wkConsole = document.getElementById("wk-console");
         var lineID = `wk-line-${strNumber}`;
         strNumber++;
 
         var formattedText = document.createElement("span");
         formattedText.setAttribute("id", lineID);
-
-        stringToPut = stringToPut.replace(/\&([0-9a-f])/g, "_ESCAPED_$1"); // Mark them uniquely
-
-        stringToPut = stringToPut.replace(/\\/g, "\\"); // Replace "\\" with "\"
         
         var parts = stringToPut.split(/(&[0-9a-f])/g);
-        var currentColor = "#ffffff"; // Default text color
+        var currentColor = "#ffffff";
 
         parts.forEach(part => {
             if (colorMap[part]) {
-                currentColor = colorMap[part]; // Apply color if valid
+                currentColor = colorMap[part];
             } else {
                 let span = document.createElement("span");
 
-                span.innerText = part.replace(/_ESCAPED_([0-9a-f])/g, "&$1");
                 span.style.color = currentColor;
                 formattedText.appendChild(span);
             }
@@ -49,11 +44,25 @@ class WebKernel {
         wkConsole.appendChild(formattedText);
         wkConsole.appendChild(document.createElement("br"));
     }
+
+    static stringOut(stringToPut) {
+        var wkConsole = document.getElementById("wk-console");
+        var lineID = `wk-line-${strNumber}`;
+        strNumber++;
+
+        var text = document.createElement("span");
+        text.setAttribute("id", lineID);
+        
+        var currentColor = "#ffffff";
+        text.style.color = currentColor;
+        text.innerText = stringToPut;
+
+        wkConsole.appendChild(formattedText);
+        wkConsole.appendChild(document.createElement("br"));
+    }
 }
 
 
 WebKernel.stringOut("Hello WebKernel!");
+WebKernel.formattedStringOut("&cThis &6is a &etest &afor &9Minecraft-like &dcolors.");
 WebKernel.stringOut("&cThis &6is a &etest &afor &9Minecraft-like &dcolors.");
-WebKernel.stringOut("\\\\ backslash!");
-WebKernel.stringOut("\\&cred text (frfr)");
-WebKernel.stringOut("\\\\&cred text");
