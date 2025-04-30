2a08ed33d354f3a58698996ae3de574595012efa
callum <master.callum.burrows@gmail.com>
2025-04-30 13:19:01 +0100

    Stuff

--- index.js
+++ index.js
@@ -1,75 +1,36 @@
-var lineNumber = 0;
-var tickNumber = 0;
-const colorMap = {
-    "&0": "#000000",
-    "&1": "#0000aa",
-    "&2": "#00aa00",
-    "&3": "#00aaaa",
-    "&4": "#aa0000",
-    "&5": "#aa00aa",
-    "&6": "#ffaa00",
-    "&7": "#aaaaaa",
-    "&8": "#555555",
-    "&9": "#5555ff",
-    "&a": "#55ff55",
-    "&b": "#55ffff",
-    "&c": "#ff5555",
-    "&d": "#ff55ff",
-    "&e": "#ffff55",
-    "&f": "#ffffff"
-};
+var shellPrefix = "> ";
 
 class WebKernel {
-    static formattedStringOut(stringToPut) {
+    static setLineText(line, stringToPut) {
         var wkConsole = document.getElementById("wk-console");
-        var lineID = `wk-line-${lineNumber}`;
-        lineNumber++;
-
-        var formattedText = document.createElement("span");
-        formattedText.setAttribute("id", lineID);
-        
-        var parts = stringToPut.split(/(&[0-9a-f])/g);
-        var currentColor = "#ffffff";
-
-        parts.forEach(part => {
-            if (colorMap[part]) {
-                currentColor = colorMap[part];
-            } else {
-                let span = document.createElement("span");
-                span.innerText = part;
-                span.style.color = currentColor;
-                formattedText.appendChild(span);
-            }
-        });
-
-        wkConsole.appendChild(formattedText);
-    }
-
-    static stringOut(stringToPut) {
-        var wkConsole = document.getElementById("wk-console");
-        var lineID = `wk-line-${lineNumber}`;
-        lineNumber++;
+        var lineID = `wk-line-${line}`;
 
         var text = document.createElement("span");
         text.setAttribute("id", lineID);
         
-        var currentColor = "#ffffff";
-        text.style.color = currentColor;
         text.innerText = stringToPut;
 
-        wkConsole.appendChild(text);
-    }
-}
+        var lineElementIfExists = document.getElementById(lineID);
+        if (lineElementIfExists == null) {
+            wkConsole.appendChild(text);
+        } else {
+            lineElementIfExists.remove();
+            wkConsole.appendChild(text);
+        };
+    };
+};
 
-function init() {
-    main()
-}
+addEventListener("keydown", (event) => {
+    // handle key presses
+});
+
+function startShell() {
+    WebKernel.setLineText(1, shellPrefix);
+};
 
 function main() {
-    WebKernel.formattedStringOut("&aWelcome &9to &eWebKernel!\n");
-    setTimeout(() => {
-        WebKernel.stringOut("This should have a delay of 2 seconds.");
-    }, 2000);
-}
+    WebKernel.setLineText(0, "Welcome to WebKernel!\n");
+    startShell()
+};
 
-init()
+main();
