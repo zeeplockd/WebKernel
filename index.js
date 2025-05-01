cfc9da6da3c4b8c08a007aaf37a6072d4de49d1c
callum <master.callum.burrows@gmail.com>
2025-05-01 10:26:01 +0100

    Made the Enter key do smth

--- index.js
+++ index.js
@@ -18,15 +18,16 @@ class WebKernel {
         }
     }
 
-    startShell() {
-        this.setLineText(1, this.shellPrefix);
+    shell() {
+        this.setLineText(this.shellLine, this.shellPrefix);
     }
 
     init() {
         this.currentInput = "";
-        
+        this.shellLine = 1;
+
         this.setLineText(0, "Welcome to WebKernel!\n");
-        this.startShell();
+        this.shell();
 
         document.addEventListener("keydown", this.handleKeyPress.bind(this));
     }
@@ -34,10 +35,13 @@ class WebKernel {
     handleKeyPress(event) {
         if (!event.repeat && event.key.length === 1) {
             this.currentInput += event.key;
-            this.setLineText(1, this.shellPrefix + this.currentInput);
-        } else if (event.key === "Backspace") { // Handle backspace
+            this.setLineText(this.shellLine, this.shellPrefix + this.currentInput);
+        } else if (event.key === "Backspace") {
             this.currentInput = this.currentInput.slice(0, -1);
-            this.setLineText(1, this.shellPrefix + this.currentInput);
+            this.setLineText(this.shellLine, this.shellPrefix + this.currentInput);
+        } else if (event.key === "Enter") {
+            this.shellLine++;
+            this.shell();
         }
     }
 }
