dfc9873d95cfc2b9961124b5583b342358a7dfd6
callum <master.callum.burrows@gmail.com>
2025-05-01 10:19:44 +0100

    Added typing and backspacing in the line

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const shellPrefix = "> ";
-
 class WebKernel {
-    constructor(consoleId) {
+    constructor(consoleId, shellPrefix) {
         this.consoleElement = document.getElementById(consoleId);
+        this.shellPrefix = shellPrefix;
     }
 
     setLineText(line, stringToPut) {
-        let lineID = `wk-line-${line}`;
-        let existingElement = document.getElementById(lineID);
+        let lineId = `wk-line-${line}`;
+        let existingElement = document.getElementById(lineId);
 
         if (existingElement) {
             existingElement.innerText = stringToPut;
         } else {
             let text = document.createElement("span");
-            text.setAttribute("id", lineID);
+            text.setAttribute("id", lineId);
             text.innerText = stringToPut;
             this.consoleElement.appendChild(text);
         }
     }
 
     startShell() {
-        this.setLineText(1, shellPrefix);
+        this.setLineText(1, this.shellPrefix);
     }
 
     init() {
+        this.currentInput = "";
+        
         this.setLineText(0, "Welcome to WebKernel!\n");
         this.startShell();
 
@@ -31,11 +32,15 @@ class WebKernel {
     }
 
     handleKeyPress(event) {
-        if (!event.repeat) {
-            console.log(`Key pressed: ${event.key}`);
-        };
+        if (!event.repeat && event.key.length === 1) {
+            this.currentInput += event.key;
+            this.setLineText(1, this.shellPrefix + this.currentInput);
+        } else if (event.key === "Backspace") { // Handle backspace
+            this.currentInput = this.currentInput.slice(0, -1);
+            this.setLineText(1, this.shellPrefix + this.currentInput);
+        }
     }
 }
 
-const kernel = new WebKernel("wk-console");
+const kernel = new WebKernel("wk-console", "> ");
 kernel.init();
